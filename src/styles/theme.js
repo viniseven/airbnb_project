8a01b5380c617e51ba3d604f33e9bfbfea16bc67
Marcus Vinicius <marcusv892@gmail.com>
2023-11-18 00:49:56 -0300

    create: theme colors

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+  COLORS: {
+    LIGHT_PURPLE: '#DDD5EA',
+    PURPLE: '#590BD8',
+    DARK_PURPLE: '#312A4F',
+    WHITE: '#FFFFFF',
+    WALTER_WHITE:'#F5F5F5',
+    LIGHT_GRAY:'#BBBFBF',
+    GRAY:'#717171',
+    RED:'#FE3838'
+  }
+}
