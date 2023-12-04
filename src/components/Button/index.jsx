fa56c7bf26076c3a43fcbfdf0879abbaf302b112
Marcus Vinicius <marcusv892@gmail.com>
2023-12-03 21:30:26 -0300

    create: component button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Button({ title, icon: Icon, ...rest }) {
+  return (
+    <Container type="button" {...rest}>
+      {Icon && <Icon size={35} />}
+      {title}
+    </Container>
+  );
+}
---
+++ src/components/Button/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  width: 100%;
+
+  border: none;
+  background: none;
+
+  svg{
+    width: 2.4rem;
+    height: auto;
+  }
+`;
